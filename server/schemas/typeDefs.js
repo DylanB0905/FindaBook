5101f75ca521e3bbafccc0d6131b36ebeafd8bc5
dburns98 <dylanburns698@gmail.com>
2022-12-17 00:01:01 -0500

    updated the auth middleware to work with graphql

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,47 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  # Input reference: https://graphql.org/graphql-js/mutations-and-input-types/
+  input BookInput {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  # Define which fields are accessible from the User model
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  # Define which fields are accessible from the Book model
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  # Define which queries the front end is allowed to make and what data is returned
+  type Query {
+    me: User
+  }
+  
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
