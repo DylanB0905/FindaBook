f403f31f029dee77d43f29fe7bdfd64a9c95f124
dburns98 <dylanburns698@gmail.com>
2022-12-17 00:47:11 -0500

    quick server fix

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const { authMiddleware } = require("./utils/auth.js")
 const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
